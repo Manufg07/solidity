9e908f514599e394ccabae55e2ebfca00fa90092
Manufg07 <manunarayananfg@gmail.com>
2024-08-23 11:54:40 +0530

    updates

---
+++ fallback.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+contract DemoF{
+    string Msg;
+    address payable public admin;
+    constructor(){
+        admin=payable(msg.sender);
+    }
+    function setMsg(string memory _msg)public{
+        Msg=_msg;
+    }
+    fallback() external payable {
+        revert();
+     }
+     receive() external payable {
+        admin.transfer(msg.value);
+      }
+
+}
