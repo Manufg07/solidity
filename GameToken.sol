0c1995f9f60e1bfdb9bbb9bb9eff887964cc2319
Manufg07 <manunarayananfg@gmail.com>
2024-09-05 15:06:13 +0530

    updates | ✨

--- GameToken.sol
+++ GameToken.sol
@@ -4,11 +4,20 @@ pragma solidity 0.8.23;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract GameT is ERC20 {
+
+     address public admin;
+
     constructor() ERC20("GameT", "GT") {
         _mint(msg.sender, 100000000000000000000);
+        admin = msg.sender;
     }
 
-    function safeMint(address to, uint value) public {
+        modifier onlyAdmin (){
+        require(msg.sender == admin,"Unautherized access");
+        _;
+    }
+
+    function safeMint(address to, uint value) public onlyAdmin {
         _mint(to, value);
     }
 
